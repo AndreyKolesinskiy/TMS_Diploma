c17e02cc66e329247acabd9e6084f0488a5032c1
Andrei Kaliasinski <andykolesinski@gmail.com>
2024-06-01 07:53:05 +0300

    Add project files.

---
+++ TMS_Diploma/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TMS_Diploma
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
